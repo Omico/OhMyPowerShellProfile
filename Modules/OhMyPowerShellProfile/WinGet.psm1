9bac65e9d1bb4a1fe43605125146def86f557390
Omico <me@omico.icu>
2023-03-05 05:29:11 -0800

    Add WinGet tab completion

--- Modules/OhMyPowerShellProfile/WinGet.psm1
+++ Modules/OhMyPowerShellProfile/WinGet.psm1
@@ -22,3 +22,17 @@ function _CheckApps($Apps, $AvailableUpgradesOutput) {
         winget upgrade $App
     }
 }
+
+# https://learn.microsoft.com/en-us/windows/package-manager/winget/tab-completion
+# @formatter:off
+# Turn off formatting because IntelliJ plugin let `--word="$Local:word"` become `--word = "$Local:word"`.
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+# @formatter:on
