bc49b6be467997c9019318f4be3a853caf58171e
Omico <me@omico.icu>
2023-04-02 13:09:38 -0700

    Generate profiles.json by Kotlin script

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Start a new terminal and run as administrator. Or, use the below command:
 Start-Process wt -verb RunAs pwsh
 ```
 
-Run the following command and modify the `profiles.json` to fit your need.
+Run the following command and modify the `profiles.json` to fit your need. For more details, please see [Customization](#customization).
 
 ```shell
 cp profiles.json.example profiles.json
@@ -38,4 +38,28 @@ cp profiles.json.example profiles.json
 
 ```powershell
 ./Install.ps1 <your-profile-id>
+```
+
+## Customization
+
+### Generate you own profile
+
+Requirements:
+
+- Kotlin
+  - Via [SDKMAN!](https://sdkman.io/): `sdk install kotlin`
+  - Via [Chocolatey](https://chocolatey.org/): `choco install kotlin`
+  - Via [Homebrew](https://brew.sh/): `brew install kotlin`
+  - Via [Scoop](https://scoop.sh/): `scoop install kotlin`
+
+```powershell
+cp profiles.example.main.kts profiles.main.kts
+```
+
+Modify the `profiles.main.kts` to fit your need. Then, run the following command to generate your own profile.
+
+```powershell
+# For example, if you installed Kotlin via Scoop，
+# the Kotlin home path is $HOME\scoop\apps\kotlin\current
+kotlinc -script profiles.main.kts -Xplugin="<your-kotlin-home>\lib\kotlinx-serialization-compiler-plugin.jar"
 ```
---
+++ profiles.example.main.kts
@@ -0,0 +1,102 @@
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+@file:Suppress("PLUGIN_IS_NOT_ENABLED")
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlin.io.path.Path
+import kotlin.io.path.writeText
+
+val exampleProfiles = Profiles(
+    profiles = listOf(
+        Profile(
+            id = "pc",
+            features = listOf(
+                "Client-ProjFS",
+                "Containers",
+                "HypervisorPlatform",
+                "Microsoft-Hyper-V-All",
+                "NetFx3",
+                "NetFx4-AdvSrvs",
+                "VirtualMachinePlatform",
+                "WorkFolders-Client",
+            ),
+            wsl = Profile.Wsl(
+                distribution = "Ubuntu",
+            ),
+            winget = Profile.Winget(
+                groups = listOf(
+                    "development",
+                    "extra",
+                ),
+                packages = listOf(
+                    "Git.Git",
+                ),
+            ),
+        ),
+    ),
+    winget = Profiles.Winget(
+        core = listOf(
+            "JanDeDobbeleer.OhMyPosh",
+            "Microsoft.OneDrive",
+            "Microsoft.PowerShell",
+            "Microsoft.VisualStudioCode",
+        ),
+        groups = listOf(
+            Profiles.Winget.Group(
+                id = "development",
+                packages = listOf(
+                    "Axosoft.GitKraken",
+                ),
+            ),
+            Profiles.Winget.Group(
+                id = "extra",
+                packages = listOf(),
+            ),
+        ),
+    ),
+)
+
+private val json = Json {
+    prettyPrint = true
+    prettyPrintIndent = "  "
+}
+
+Path("profiles.json.example").writeText(json.encodeToString(exampleProfiles))
+
+@Serializable
+data class Profiles(
+    val profiles: List<Profile> = listOf(),
+    val winget: Winget,
+) {
+    @Serializable
+    data class Winget(
+        val core: List<String>,
+        val groups: List<Group> = listOf(),
+    ) {
+        @Serializable
+        data class Group(
+            val id: String,
+            val packages: List<String> = listOf(),
+        )
+    }
+}
+
+@Serializable
+data class Profile(
+    val id: String,
+    val features: List<String> = listOf(),
+    val wsl: Wsl = Wsl(),
+    val winget: Winget,
+) {
+    @Serializable
+    data class Wsl(
+        val distribution: String = "Ubuntu",
+    )
+
+    @Serializable
+    data class Winget(
+        val groups: List<String> = listOf(),
+        val packages: List<String> = listOf(),
+    )
+}
